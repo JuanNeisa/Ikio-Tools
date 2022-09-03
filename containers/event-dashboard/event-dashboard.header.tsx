a9c1285802d9784ec2bf51f5f7ff2e2f285a2198
Juan David Neisa <neisaparamo@hotmail.com>
2022-09-03 13:39:17 -0500

    Feature: Cambio en dashboard. Refactorizacion status bar

--- containers/event-dashboard/event-dashboard.header.tsx
+++ containers/event-dashboard/event-dashboard.header.tsx
@@ -1,16 +1,11 @@
 import * as _ from "lodash";
-import { useState } from "react";
-import Modal_box from "../../components/modal";
-import { Status_bar } from "../../components/status-bar"
 import { IEvento } from "../../core/models/database.model"
-import { PilotsModal } from "./modals/pilotsModal";
 
 interface Props {
     data: IEvento
 }
 
 export function EventDashboard_Header(props: Props) {
-    const [showPilotsModal, setShowPilotModal] = useState(false);
     return(
         <div className="border border-3 p-4 rounded-3">
             <h4><i className="bi bi-info-square me-2"></i>Informacion</h4>
@@ -30,25 +25,6 @@ export function EventDashboard_Header(props: Props) {
                 <span className="col-4 fw-bolder">Descripcion</span>
                 <span className="col-8">{(!_.isNil(props.data))?props.data.descripcion:'Sin descripcion'}</span>
             </div>
-            {/* <div className="row m-2 ">
-                <span className="col-4 fw-bolder ">Status</span>
-                <div className="col-8">
-                    { props.data &&
-                        <Status_bar data={[
-                            {title: 'Configuracion' , onClick: ()=>{console.log('Configuracion')}},
-                            {title: 'Pilotos' , onClick: () => {setShowPilotModal(true)}},
-                            {title: 'Carreras' , onClick: ()=>{console.log('Carreras')}},
-                            {title: 'Informes' , onClick: ()=>{console.log('Informes')}}
-                        ]}
-                        state={props.data.estado} />
-                    }
-                </div>
-            </div> */}
-            <Modal_box 
-                title={"Agregar pilotos"} 
-                body={<PilotsModal />} 
-                isShow={showPilotsModal} 
-                onClose={() => setShowPilotModal(false)} />
         </div>
     )
 }
--- containers/event-dashboard/index.tsx
+++ containers/event-dashboard/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+import Modal_box from "../../components/modal";
 import { IEvento } from "../../core/models/database.model";
 import { EventDashboard_Header } from "./event-dashboard.header";
+import { PilotsModal } from "./modals/pilotsModal";
 
 interface Props {
     data: IEvento
 }
 
 export function EventDashboard(props: Props) {
+    const [showPilotsModal, setShowPilotModal] = useState(false);
+    
     return(
         <>
             <div className="container">
@@ -16,31 +21,50 @@ export function EventDashboard(props: Props) {
                     <div className="col-4">
                         <div className="card border-secondary mb-1">
                             <div className="card-body text-secondary d-flex flex-column">
-                                <h5 className="card-title">Configuracion</h5>
+                                <div className="d-flex justify-content-between">
+                                    <h5 className="card-title">Configuracion</h5>
+                                    <i className="bi bi-check-lg" style={{color: 'green'}}></i>
+                                </div>
                                 <button type="button" className="btn btn-secondary align-content-end">Editar</button>
                             </div>
                         </div>
                         <div className="card border-secondary mb-1">
                             <div className="card-body text-secondary d-flex flex-column">
-                                <h5 className="card-title">Pilotos</h5>
+                            <div className="d-flex justify-content-between">
+                                    <h5 className="card-title">Pilotos</h5>
+                                    <i className="bi bi-x-lg" style={{color: 'red'}}></i>
+                                </div>
+                                <button type="button" 
+                                        className="btn btn-secondary align-content-end"
+                                        onClick={() => {setShowPilotModal(true)}}>Cargar</button>
+                            </div>
+                        </div>
+                        <div className="card border-secondary mb-1">
+                            <div className="card-body text-secondary d-flex flex-column">
+                            <div className="d-flex justify-content-between">
+                                    <h5 className="card-title">Carreras</h5>
+                                    <i className="bi bi-x-lg" style={{color: 'red'}}></i>
+                                </div>
                                 <button type="button" className="btn btn-secondary align-content-end">Cargar</button>
                             </div>
                         </div>
                         <div className="card border-secondary mb-1">
                             <div className="card-body text-secondary d-flex flex-column">
-                                <h5 className="card-title">Carreras</h5>
-                                <button type="button" className="btn btn-secondary align-content-end">Cargar</button>
-                            </div>
-                        </div>
-                        <div className="card border-secondary mb-1">
-                            <div className="card-body text-secondary d-flex flex-column">
-                                <h5 className="card-title">Informes</h5>
+                            <div className="d-flex justify-content-between">
+                                    <h5 className="card-title">Informes</h5>
+                                    <i className="bi bi-x-lg" style={{color: 'red'}}></i>
+                                </div>
                                 <button type="button" className="btn btn-secondary align-content-end disabled">Generar</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Modal_box 
+                title={"Agregar pilotos"} 
+                body={<PilotsModal />} 
+                isShow={showPilotsModal} 
+                onClose={() => setShowPilotModal(false)} />
         </>
     )
 }
